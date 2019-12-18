5fa1ae294379e5c8d4d815cfad4678507f58011c
sinaraarliss <sinaramartins@gmail.com>
2019-12-18 11:34:58 -0500

    Added condition to check if user leaves input blank. If it's blank then getFacts returns a random fact

--- src/services/Api-helper.js
+++ src/services/Api-helper.js
@@ -4,29 +4,34 @@ import axios from 'axios';
 export const getFacts = async (category, value) => {
   let response = "";
 
-  switch (category) {
-    case "number":
-      response = await axios.get(`http://numbersapi.com/${value}`);
-      return (response);
+  /* If user leaves input field empty then value is false and it returns a ramdon fact */
+    if (value) {
+      switch (category) {
+        case "number":
+          response = await axios.get(`http://numbersapi.com/${value}`);
+          return (response);
 
-    case "year":
-      response = await axios.get(`http://numbersapi.com/${value}/year`);
-      return (response);
+        case "year":
+          response = await axios.get(`http://numbersapi.com/${value}/year`);
+          return (response);
 
-    case "date":
-      response = await axios.get(`http://numbersapi.com/${value}/date`);
-      return (response);
+        case "date":
+          response = await axios.get(`http://numbersapi.com/${value}/date`);
+          return (response);
 
-    case "math":
-      response = await axios.get(`http://numbersapi.com/${value}/math`);
-      return (response);
+        case "math":
+          response = await axios.get(`http://numbersapi.com/${value}/math`);
+          return (response);
 
-    case "random":
+        default:
+          response = await axios.get(`http://numbersapi.com/random`);
+          return (response);
+      }
+    } else {
       response = await axios.get(`http://numbersapi.com/random`);
       return (response);
-
-    default:
-      response = await axios.get(`http://numbersapi.com/random`);
-      return (response);
+    }
   }
-}
+
+
+
