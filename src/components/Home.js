c0a431254d26b8a3301658d6520b2fc0be258a53
sinaraarliss <sinaramartins@gmail.com>
2019-12-17 19:53:10 -0500

    Updated Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,29 +4,33 @@ import { Link } from "react-router-dom";
 function Home() {
 
   return (
-    <div className="front-categories">
+    <div>
+      <h4>Choose a category:</h4>
 
-      <div>
-        <Link to="/Number">Number</Link>
+      <div className="front-categories">
+
+        <div>
+          <Link to="/Number">Number</Link>
+        </div>
+
+        <div>
+          <Link to="/#">Date</Link>
+        </div>
+
+        <div>
+          <Link to="/#">Year</Link>
+        </div>
+
+        <div>
+          <Link to="/#">Math</Link>
+        </div>
+
+        <div>
+          <Link to="/#">Random</Link>
+        </div>
+
+
       </div>
-
-      <div>
-        <Link to="/#">Date</Link>
-      </div>
-
-      <div>
-        <Link to="/#">Year</Link>
-      </div>
-
-      <div>
-        <Link to="/#">Math</Link>
-      </div>
-
-      <div>
-        <Link to="/#">Random</Link>
-      </div>
-
-
     </div>
   )
 }
